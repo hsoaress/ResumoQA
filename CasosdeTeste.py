1d64019402c727e19a3160de4aa8db2b60729952
Heloisa Samantha <129108267+hsoaress@users.noreply.github.com>
2025-05-22 19:57:40 -0300

    Update CasosdeTeste.py

--- CasosdeTeste.py
+++ CasosdeTeste.py
@@ -9,3 +9,15 @@ if usuario_logado:
 assert carrinho["total"] == 30, "Total incorreto no carrinho!"
 
 print("Critério de aceitação passou!")
+
+def realizar_pagamento(saldo, valor_compra):
+    if saldo >= valor_compra:
+        return "Pagamento aprovado"
+    else:
+        return "Saldo insuficiente"
+
+# Teste baseado em risco (pagamento é crítico)
+assert realizar_pagamento(100, 50) == "Pagamento aprovado"
+assert realizar_pagamento(50, 100) == "Saldo insuficiente"
+
+print("Testes críticos de pagamento passaram!")
