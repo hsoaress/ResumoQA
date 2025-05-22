89dbd83016080ba34deb546dc8a4569796b09460
Heloisa Samantha <129108267+hsoaress@users.noreply.github.com>
2025-05-22 20:23:18 -0300

    Planejamento e Estratégias de Teste

---
+++ metricas.py
@@ -0,0 +1,9 @@
+# Métricas: simulação de cobertura
+
+# Simulando cobertura de testes
+funcoes_totais = 5
+funcoes_testadas = 4
+
+cobertura = (funcoes_testadas / funcoes_totais) * 100
+
+print(f"Taxa de cobertura: {cobertura}%")  # Ex.: 80%
