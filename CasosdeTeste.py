69232341fcc50eeb4de26e29c41f5b5cdcda5ac0
Heloisa Samantha <129108267+hsoaress@users.noreply.github.com>
2025-05-22 19:55:51 -0300

    Create CasosdeTeste.py

---
+++ CasosdeTeste.py
@@ -0,0 +1,11 @@
+# Cenário: "Dado um usuário logado, Quando adiciona um item ao carrinho, Então o total deve ser atualizado"
+
+usuario_logado = True
+carrinho = {"total": 0}
+
+if usuario_logado:
+    carrinho["total"] += 30
+
+assert carrinho["total"] == 30, "Total incorreto no carrinho!"
+
+print("Critério de aceitação passou!")
