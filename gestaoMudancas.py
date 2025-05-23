6daaf1205009e862d5857554cabe556e0a02eaab
Heloisa Samantha <129108267+hsoaress@users.noreply.github.com>
2025-05-23 08:50:02 -0300

    Update gestaoMudancas.py

--- gestaoMudancas.py
+++ gestaoMudancas.py
@@ -6,3 +6,9 @@ scripts_teste = ["v1 - Teste inicial", "v2 - Teste com novos cenários"]
 for versao in scripts_teste:
     print(f"Script de teste: {versao}")
     
+
+
+
+#Saída:
+    #Script de teste: v1 - Teste inicial
+    #Script de teste: v2 - Teste com novos cenários
