1026b45dba27a7e8f980e50bb4409e2781f1b939
Heloisa Samantha <129108267+hsoaress@users.noreply.github.com>
2025-05-23 08:54:11 -0300

    Update testeAPI.py

--- testeAPI.py
+++ testeAPI.py
@@ -6,3 +6,7 @@ resposta = requests.get(url)
 
 assert resposta.status_code == 200, "Falha na requisição"
 print("Requisição bem-sucedida:", resposta.json())
+
+#Saída: 
+     #Requisição bem-sucedida: {'userId': 1, 'id': 1, 'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', 'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
+
